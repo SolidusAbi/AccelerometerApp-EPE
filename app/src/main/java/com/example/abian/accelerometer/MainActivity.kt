4bfeeb3ae032e960c8f4c9d7da70ac294f468f40
Abian Hernandez <abianhernandez@outlook.es>
2018-10-22 08:47:04 +0100

    Access to accelerometer manager, the information is displayed in a text view

---
+++ app/src/main/java/com/example/abian/accelerometer/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.abian.accelerometer
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    lateinit var sensorManager: SensorManager
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        accelerometer_data.text  = "x = ${event!!.values[0]}\n\n" +
+                "y = ${event.values[1]}\n\n" +
+                "z = ${event.values[2]}"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager.registerListener(
+            this,
+            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+            SensorManager.SENSOR_DELAY_NORMAL
+
+        )
+
+        /*val filename = "myfile"
+        val fileContents = "Hello world!"
+        context.openFileOutput(filename, Context.MODE_PRIVATE).use {
+            it.write(fileContents.toByteArray())
+        }*/
+    }
+}
